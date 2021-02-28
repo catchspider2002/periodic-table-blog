ab13fe0dc1313bd8e8d6a767e4352e25925b43ad
Naveen <catchspider2002@gmail.com>
2021-02-28 12:06:28 +0530

    Testing og image

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -12,6 +12,16 @@
   export let posts;
 
   import NavBar from "../components/NavBar.svelte";
+
+  
+  let website = "https://blog.periodic-table.io";
+  let title = "Blog - Periodic-table.io";
+  let page = "/downloads/";
+  let url = website + page;
+  let keywords = "download, free downloads";
+  let desc = "Downloads";
+  let image = website + "/bitmap.png";
+
 </script>
 
 <style>
@@ -23,6 +33,21 @@
 
 <svelte:head>
   <title>Blog</title>
+  
+  <!-- Open Graph / Facebook -->
+  <meta property="og:url" content={url} />
+  <meta property="og:title" content={title} />
+  <meta property="og:description" content={desc} />
+  <meta property="og:image" content={image} />
+
+  <!-- Twitter -->
+  <meta name="twitter:url" content={url} />
+  <meta name="twitter:title" content={title} />
+  <meta name="twitter:description" content={desc} />
+  <meta name="twitter:image" content={image} />
+  <meta name="twitter:image:src" content={image} />
+  <meta name="twitter:image:alt" content={title} />
+
 </svelte:head>
 <NavBar />
 <h1>Recent posts</h1>
